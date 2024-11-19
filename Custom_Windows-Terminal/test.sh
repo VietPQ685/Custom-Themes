7ebb65ffaf00f2f14bb7f26832ca75000f7636fc
Phùng Quốc Việt <92231102+VietPQ685@users.noreply.github.com>
2024-11-20 00:50:26 +0700

    Update test.sh

--- Custom_Windows-Terminal/test.sh
+++ Custom_Windows-Terminal/test.sh
@@ -3,5 +3,5 @@
 echo 'Setting locale'
 echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
 sudo locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "LANG=en_US.UTF-8" | sudo tee -a /etc/locale.conf
 export LANG=en_US.UTF-8

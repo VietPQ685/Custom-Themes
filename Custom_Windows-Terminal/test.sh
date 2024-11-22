53bf7edd64ea22a89eda86a1f0ac01b1639377e9
Phùng Quốc Việt <92231102+vietpq685@users.noreply.github.com>
2024-11-23 03:37:50 +0700

    Delete Custom_Windows-Terminal/test.sh

--- Custom_Windows-Terminal/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-echo 'Setting locale'
-echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
-sudo locale-gen
-echo "LANG=en_US.UTF-8" | sudo tee -a /etc/locale.conf
-export LANG=en_US.UTF-8
